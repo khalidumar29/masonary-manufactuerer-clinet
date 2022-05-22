0a42928e3d0d9d950fed79dd88118831a529ba35
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-22 12:23:41 +0600

    initlize navbar with daisy ui and deploy firebase successfuly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Shared/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-green-500'>intialize project</h1>
+    <div className='lg:mx-8'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className='mx-6'>
+      <h1 className='text-[black]'>this is home page </h1>
+    </div>
+  );
+};
+
+export default Home;
