70970c8933ebf9f83b5c8e43e594b118bf60296d
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-27 21:47:12 +0600

    make admin done

--- src/Pages/Dashboard/AllUser/AllUser.js
+++ src/Pages/Dashboard/AllUser/AllUser.js
@@ -1,15 +1,34 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { useQuery } from "react-query";
 import Loading from "../../../Shared/Loading/Loading";
 
 const AllUser = () => {
-  const { isLoading, data: users } = useQuery("user", () =>
+  const {
+    isLoading,
+    data: users,
+    refetch,
+  } = useQuery("user", () =>
     fetch("http://localhost:5000/user").then((res) => res.json())
   );
   if (isLoading) {
     return <Loading />;
   }
-
+  const makeAdmin = (email) => {
+    console.log(email);
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json)
+      .then((data) => {
+        refetch();
+        console.log(data);
+        toast.success(`Successfully made an admin`);
+      });
+  };
   return (
     <div>
       <h1 className='text-center text-2xl text-primary my-4'>All User</h1>
@@ -24,12 +43,16 @@ const AllUser = () => {
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <tr>
+              <tr key={user._key}>
                 <th>{index + 1}</th>
                 <td>{user.email}</td>
                 <td>
-                  <button className='btn btn-xs'>make admin</button>
-                  <button className='btn btn-xs ml-2'>delete</button>
+                  <button
+                    onClick={() => makeAdmin(user.email)}
+                    className='btn btn-xs'
+                  >
+                    make admin
+                  </button>
                 </td>
               </tr>
             ))}
